dfa08bd6fff1ca2bbe35ffaa72f5c0c83731108b
dmitry ragozin <dmtryragozin@gmail.com>
2025-05-27 21:41:09 +0300

    refactor: code style

--- src/helpers/getMongoDbConnectString.ts
+++ src/helpers/getMongoDbConnectString.ts
@@ -1,3 +1,11 @@
-const getMongoDbConnectString = ({ dbHost, dbPort, dbName }: {dbHost?: string, dbPort?: string, dbName?: string}) => `mongodb://${dbHost}:${dbPort}/${dbName}`;
+const getMongoDbConnectString = ({
+  dbHost,
+  dbPort,
+  dbName,
+}: {
+  dbHost?: string,
+  dbPort?: string,
+  dbName?: string
+}) => `mongodb://${dbHost}:${dbPort}/${dbName}`;
 
 export default getMongoDbConnectString;
