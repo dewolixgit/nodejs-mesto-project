c6105582e4a2c88c396f40252fcde274cd0ca14f
dmitry ragozin <dmtryragozin@gmail.com>
2025-05-27 21:49:40 +0300

    fix: удалить лишнее

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { errors as celebrateErrors } from 'celebrate';
